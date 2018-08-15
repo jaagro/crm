9c1bcc64cbdb1ae78dd14689df6efbe89d5b5a36
baiyiran <1169172027@qq.com>
2018-08-15 16:21:00 +0800

    代码规范优化

---
+++ crm-web/src/main/java/com/jaagro/crm/web/CrmApplication.java
@@ -0,0 +1,25 @@
+package com.jaagro.crm.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author tony
+ */
+@EnableEurekaClient
+@EnableCircuitBreaker
+@MapperScan("com.jaagro.crm.biz.mapper")
+@ComponentScan("com.jaagro.crm")
+@EnableFeignClients(basePackages = {"com.jaagro.crm.api"})
+@SpringBootApplication
+public class CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
+}
