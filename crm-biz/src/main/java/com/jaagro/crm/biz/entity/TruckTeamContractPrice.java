bf88016d8345489afdea93b0d5e77fa65bb414a1
zhengxingtao <zheng_xigntao@126.com>
2018-09-04 17:56:46 +0800

    更新车队合同报价

--- crm-biz/src/main/java/com/jaagro/crm/biz/entity/TruckTeamContractPrice.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/entity/TruckTeamContractPrice.java
@@ -1,21 +1,38 @@
 package com.jaagro.crm.biz.entity;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class TruckTeamContractPrice {
+/**
+ * @author tony
+ */
+@Data
+@Accessors(chain = true)
+public class TruckTeamContractPrice implements Serializable {
     /**
      * 
      */
     private Integer id;
 
     /**
-     * 合同id Nullable References: contract
+     * 合同id
+ Nullable
+ References: contract
      */
     private Integer truckTeamContractId;
 
     /**
-     * 计价方式 说明： 1、按重量;  2、按数量; 3、按重量＊里程；  4，按数量＊里程 Nullable
+     * 计价方式
+ 说明：
+ 1、按重量; 
+ 2、按数量;
+ 3、按重量＊里程； 
+ 4，按数量＊里程
+ Nullable
      */
     private Integer pricingType;
 
@@ -30,7 +47,8 @@ public class TruckTeamContractPrice {
     private Integer productType;
 
     /**
-     * 单价 Nullable
+     * 单价
+ Nullable
      */
     private BigDecimal price;
 
@@ -48,148 +66,4 @@ public class TruckTeamContractPrice {
      * 报价截止时间
      */
     private Date endDate;
-
-    /**
-     * 
-     * @return id 
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * 
-     * @param id 
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * 合同id Nullable References: contract
-     * @return truck_team_contract_id 合同id Nullable References: contract
-     */
-    public Integer getTruckTeamContractId() {
-        return truckTeamContractId;
-    }
-
-    /**
-     * 合同id Nullable References: contract
-     * @param truckTeamContractId 合同id Nullable References: contract
-     */
-    public void setTruckTeamContractId(Integer truckTeamContractId) {
-        this.truckTeamContractId = truckTeamContractId;
-    }
-
-    /**
-     * 计价方式 说明： 1、按重量;  2、按数量; 3、按重量＊里程；  4，按数量＊里程 Nullable
-     * @return pricing_type 计价方式 说明： 1、按重量;  2、按数量; 3、按重量＊里程；  4，按数量＊里程 Nullable
-     */
-    public Integer getPricingType() {
-        return pricingType;
-    }
-
-    /**
-     * 计价方式 说明： 1、按重量;  2、按数量; 3、按重量＊里程；  4，按数量＊里程 Nullable
-     * @param pricingType 计价方式 说明： 1、按重量;  2、按数量; 3、按重量＊里程；  4，按数量＊里程 Nullable
-     */
-    public void setPricingType(Integer pricingType) {
-        this.pricingType = pricingType;
-    }
-
-    /**
-     * 车辆类型
-     * @return vehicle_type 车辆类型
-     */
-    public Integer getVehicleType() {
-        return vehicleType;
-    }
-
-    /**
-     * 车辆类型
-     * @param vehicleType 车辆类型
-     */
-    public void setVehicleType(Integer vehicleType) {
-        this.vehicleType = vehicleType;
-    }
-
-    /**
-     * 货物类型
-     * @return product_type 货物类型
-     */
-    public Integer getProductType() {
-        return productType;
-    }
-
-    /**
-     * 货物类型
-     * @param productType 货物类型
-     */
-    public void setProductType(Integer productType) {
-        this.productType = productType;
-    }
-
-    /**
-     * 单价 Nullable
-     * @return price 单价 Nullable
-     */
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    /**
-     * 单价 Nullable
-     * @param price 单价 Nullable
-     */
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    /**
-     * 是否有效
-     * @return price_status 是否有效
-     */
-    public Boolean getPriceStatus() {
-        return priceStatus;
-    }
-
-    /**
-     * 是否有效
-     * @param priceStatus 是否有效
-     */
-    public void setPriceStatus(Boolean priceStatus) {
-        this.priceStatus = priceStatus;
-    }
-
-    /**
-     * 报价生效时间
-     * @return start_date 报价生效时间
-     */
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    /**
-     * 报价生效时间
-     * @param startDate 报价生效时间
-     */
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    /**
-     * 报价截止时间
-     * @return end_date 报价截止时间
-     */
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    /**
-     * 报价截止时间
-     * @param endDate 报价截止时间
-     */
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
 }
--- crm-biz/src/main/java/com/jaagro/crm/biz/entity/TruckTeamContractSectionPrice.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/entity/TruckTeamContractSectionPrice.java
@@ -1,8 +1,17 @@
 package com.jaagro.crm.biz.entity;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class TruckTeamContractSectionPrice {
+/**
+ * @author tony
+ */
+@Data
+@Accessors(chain = true)
+public class TruckTeamContractSectionPrice implements Serializable {
     /**
      * 
      */
@@ -37,116 +46,4 @@ public class TruckTeamContractSectionPrice {
      * 是否有效 1-有效，0-无效
      */
     private Integer selectionStatus;
-
-    /**
-     * 
-     * @return id 
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * 
-     * @param id 
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * 
-     * @return contract_price_id 
-     */
-    public Integer getContractPriceId() {
-        return contractPriceId;
-    }
-
-    /**
-     * 
-     * @param contractPriceId 
-     */
-    public void setContractPriceId(Integer contractPriceId) {
-        this.contractPriceId = contractPriceId;
-    }
-
-    /**
-     * 1-重量阶梯报价 2-里程阶梯报价
-     * @return section_type 1-重量阶梯报价 2-里程阶梯报价
-     */
-    public Integer getSectionType() {
-        return sectionType;
-    }
-
-    /**
-     * 1-重量阶梯报价 2-里程阶梯报价
-     * @param sectionType 1-重量阶梯报价 2-里程阶梯报价
-     */
-    public void setSectionType(Integer sectionType) {
-        this.sectionType = sectionType;
-    }
-
-    /**
-     * 阶梯下限
-     * @return upper_limit 阶梯下限
-     */
-    public BigDecimal getUpperLimit() {
-        return upperLimit;
-    }
-
-    /**
-     * 阶梯下限
-     * @param upperLimit 阶梯下限
-     */
-    public void setUpperLimit(BigDecimal upperLimit) {
-        this.upperLimit = upperLimit;
-    }
-
-    /**
-     * 阶梯上限
-     * @return lower_limit 阶梯上限
-     */
-    public BigDecimal getLowerLimit() {
-        return lowerLimit;
-    }
-
-    /**
-     * 阶梯上限
-     * @param lowerLimit 阶梯上限
-     */
-    public void setLowerLimit(BigDecimal lowerLimit) {
-        this.lowerLimit = lowerLimit;
-    }
-
-    /**
-     * 单价
-     * @return price 单价
-     */
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    /**
-     * 单价
-     * @param price 单价
-     */
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    /**
-     * 是否有效 1-有效，0-无效
-     * @return selection_status 是否有效 1-有效，0-无效
-     */
-    public Integer getSelectionStatus() {
-        return selectionStatus;
-    }
-
-    /**
-     * 是否有效 1-有效，0-无效
-     * @param selectionStatus 是否有效 1-有效，0-无效
-     */
-    public void setSelectionStatus(Integer selectionStatus) {
-        this.selectionStatus = selectionStatus;
-    }
 }
