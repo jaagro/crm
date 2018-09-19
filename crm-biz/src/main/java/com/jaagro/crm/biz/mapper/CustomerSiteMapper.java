5866a56820a60bd47599771c3a24ec5a1d2b5710
zhengxingtao <zheng_xigntao@126.com>
2018-09-19 15:31:49 +0800

    更新ext

--- crm-biz/src/main/java/com/jaagro/crm/biz/mapper/CustomerSiteMapper.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/mapper/CustomerSiteMapper.java
@@ -1,14 +1,6 @@
 package com.jaagro.crm.biz.mapper;
 
-import com.jaagro.crm.api.dto.request.customer.ListSiteCriteriaDto;
-import com.jaagro.crm.api.dto.request.customer.ShowSiteDto;
-import com.jaagro.crm.api.dto.request.customer.UpdateCustomerSiteDto;
-import com.jaagro.crm.api.dto.response.customer.CustomerSiteReturnDto;
-import com.jaagro.crm.api.dto.response.selectValue.ReturnSelectSiteDto;
 import com.jaagro.crm.biz.entity.CustomerSite;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 public interface CustomerSiteMapper {
     /**
