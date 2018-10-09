7553ab288707ae585130e55c8c9fe62dfc473c9b
baiyiran <1169172027@qq.com>
2018-10-09 14:09:59 +0800

    重新上传审核不通过的资质bug修复

--- crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/TruckQualificationServiceImpl.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/TruckQualificationServiceImpl.java
@@ -120,7 +120,13 @@ public class TruckQualificationServiceImpl implements TruckQualificationService 
                             .setCertificateStatus(AuditStatus.STOP_COOPERATION);
                     this.truckQualificationMapper.updateByPrimaryKeySelective(truckQualification);
                     // 把新资质证件照新增
-                    qualification.setCertificateStatus(AuditStatus.UNCHECKED);
+                    qualification
+                            .setCreateUserId(currentUserService.getCurrentUser().getId())
+                            .setTruckTeamId(truckQualification.getTruckTeamId())
+                            .setTruckId(truckQualification.getTruckId())
+                            .setDriverId(truckQualification.getDriverId())
+                            .setNotes(truckQualification.getNotes())
+                            .setCertificateStatus(AuditStatus.UNCHECKED);
                     this.truckQualificationMapper.insertSelective(qualification);
                     return ServiceResult.toResult("操作成功");
                 }
