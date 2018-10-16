f6caf13d863d65281be9a762300b6ccf01e500c0
baiyiran <1169172027@qq.com>
2018-10-16 09:43:25 +0800

    新增查询单个资质的接口（包括详细信息）【运力，客户，合同】bug修复

--- crm-web/src/main/java/com/jaagro/crm/web/controller/TruckQualificationController.java
+++ crm-web/src/main/java/com/jaagro/crm/web/controller/TruckQualificationController.java
@@ -103,7 +103,7 @@ public class TruckQualificationController {
 
     @ApiOperation("查询单个运力资质【包括详细信息】")
     @GetMapping("/getQuaById/{id}")
-    public BaseResponse getQualificationByd(@PathVariable Integer id) {
+    public BaseResponse getQuaById(@PathVariable Integer id) {
         ReturnTruckQualificationDto qualificationDto = this.truckQualificationMapper.getById(id);
         if (qualificationDto != null) {
             //填充司机信息
