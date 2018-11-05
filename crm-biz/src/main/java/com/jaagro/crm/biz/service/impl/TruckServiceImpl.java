9fd25f3c7c32942bd50257c4ce2c110043955cb7
baiyiran <1169172027@qq.com>
2018-11-05 15:58:07 +0800

    bug修复

--- crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/TruckServiceImpl.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/TruckServiceImpl.java
@@ -78,6 +78,7 @@ public class TruckServiceImpl implements TruckService {
     @Override
     @Cacheable
     public Map<String, Object> getTruckById(Integer truckId) {
+        Truck truck = truckMapper.selectByPrimaryKey(truckId);
         GetTruckDto result = truckMapper.getTruckById(truckId);
         if (StringUtils.isEmpty(result)) {
             return ServiceResult.error(ResponseStatusCode.QUERY_DATA_EMPTY.getCode(), "车辆不存在");
@@ -100,7 +101,7 @@ public class TruckServiceImpl implements TruckService {
         }
         result
                 .setDrivers(drivers)
-                .setTruckTypeId(this.truckTypeMapper.getById(result.getTruckTypeId().getId()));
+                .setTruckTypeId(this.truckTypeMapper.getById(truck.getId()));
         return ServiceResult.toResult(result);
     }
 
