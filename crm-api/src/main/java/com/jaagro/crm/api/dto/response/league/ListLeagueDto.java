d3b8e2d995db191b5d36d353a771a662e7fe7a60
baiyiran <1169172027@qq.com>
2018-11-14 18:40:45 +0800

    新增销售机会，分页列表

--- crm-api/src/main/java/com/jaagro/crm/api/dto/response/league/ListLeagueDto.java
+++ crm-api/src/main/java/com/jaagro/crm/api/dto/response/league/ListLeagueDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author baiyiran
@@ -12,4 +13,38 @@ import java.io.Serializable;
 @Data
 @Accessors(chain = true)
 public class ListLeagueDto implements Serializable {
+    /**
+     * 主键id
+     */
+    private Integer id;
+
+    /**
+     * 销售机会类型
+     */
+    private String leagueType;
+
+    /**
+     * 姓名
+     */
+    private String leagueName;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 企业名称
+     */
+    private String company;
+
+    /**
+     * 详细内容
+     */
+    private String content;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
 }
