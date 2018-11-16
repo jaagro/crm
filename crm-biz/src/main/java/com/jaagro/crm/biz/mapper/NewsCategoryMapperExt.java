2f75ae81e7a531eaef7a74cd2b60dbe0797c8222
yogasan <yoga330356233@126.com>
2018-11-16 11:37:08 +0800

    客户,司机审核通过创建账户,删除客户时删除账户

---
+++ crm-biz/src/main/java/com/jaagro/crm/biz/mapper/NewsCategoryMapperExt.java
@@ -0,0 +1,8 @@
+package com.jaagro.crm.biz.mapper;
+
+/**
+ * @author yj
+ * @date 2018/11/16
+ */
+public interface NewsCategoryMapperExt extends NewsCategoryMapper {
+}
---
+++ crm-biz/src/main/java/com/jaagro/crm/biz/mapper/NewsMapperExt.java
@@ -0,0 +1,8 @@
+package com.jaagro.crm.biz.mapper;
+
+/**
+ * @author yj
+ * @since 20181116
+ */
+public interface NewsMapperExt extends NewsMapper {
+}
