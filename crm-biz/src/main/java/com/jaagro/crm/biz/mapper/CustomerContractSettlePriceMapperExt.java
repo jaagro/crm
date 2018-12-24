089cba24b33ae6a8c6cdebd75248ead6dca56b45
yogasan <yoga330356233@126.com>
2018-12-24 17:49:16 +0800

    报价算法api

---
+++ crm-biz/src/main/java/com/jaagro/crm/biz/mapper/CustomerContractSettlePriceMapperExt.java
@@ -0,0 +1,9 @@
+package com.jaagro.crm.biz.mapper;
+
+/**
+ * 客户合同结算价格CRUD扩展
+ * @author yj
+ * @since 2018/12/24
+ */
+public interface CustomerContractSettlePriceMapperExt extends CustomerContractSettlePriceMapper {
+}
