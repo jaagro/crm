76d32cdf0bc49fcba3642eb569eabbeaee337813
baiyiran <1169172027@qq.com>
2018-12-26 15:57:51 +0800

    客户合同新报价-基础方法2

--- crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/CtmContractSettleSectionRuleServiceImpl.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/CtmContractSettleSectionRuleServiceImpl.java
@@ -2,11 +2,15 @@ package com.jaagro.crm.biz.service.impl;
 
 import com.jaagro.crm.api.dto.request.customer.CreateCustomerSectionRuleDto;
 import com.jaagro.crm.api.service.CtmContractSettleSectionRuleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 /**
  * @author baiyiran
  * @Date 2018/12/26
  */
+@Service
+@Slf4j
 public class CtmContractSettleSectionRuleServiceImpl implements CtmContractSettleSectionRuleService {
 
 
