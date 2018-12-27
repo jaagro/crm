2e356cf89b49bef700bd2547bcba80ed5655d70b
yogasan <yoga330356233@126.com>
2018-12-27 17:20:37 +0800

    司机报价算法

---
+++ crm-api/src/main/java/com/jaagro/crm/api/constant/PricingMethod.java
@@ -0,0 +1,21 @@
+package com.jaagro.crm.api.constant;
+
+/**
+ * 司机合同结算配制计价方式
+ * @author yj
+ * @since 2018/12/25
+ */
+public final class PricingMethod {
+    /**
+     * 按区间重量(饲料)
+     */
+    public static final Integer SECTION_WEIGHT = 1;
+    /**
+     * 按区间里程(毛鸡/仔猪/生猪)
+     */
+    public static final Integer SECTION_MILEAGE = 2;
+    /**
+     * 按起步里程+里程单价(仔猪/生猪)
+     */
+    public static final Integer BEGIN_WEIGHT = 3;
+}
--- crm-biz/src/main/java/com/jaagro/crm/biz/mapper/DriverContractSettleRuleMapperExt.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/mapper/DriverContractSettleRuleMapperExt.java
@@ -1,9 +1,21 @@
 package com.jaagro.crm.biz.mapper;
 
+import com.jaagro.crm.biz.entity.DriverContractSettleRule;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
 /**
  * 司机合同结算配制CRUD扩展
  * @author yj
  * @since 2018/12/24
  */
 public interface DriverContractSettleRuleMapperExt extends DriverContractSettleRuleMapper{
+    /**
+     * 根据车队合同id,车辆类型id,完成时间查询有效的司机合同结算配制
+     * @param truckTeamContractId
+     * @param truckTypeId
+     * @param doneDate
+     * @return
+     */
+    DriverContractSettleRule selectEffectiveOne(@Param("truckTeamContractId") Integer truckTeamContractId, @Param("truckTypeId") Integer truckTypeId, @Param("doneDate") Date doneDate);
 }
