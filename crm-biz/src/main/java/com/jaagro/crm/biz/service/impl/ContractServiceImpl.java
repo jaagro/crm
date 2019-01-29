9b0c6aa4c25bb9c79639a7e312146f03281b8c94
baiyiran <1169172027@qq.com>
2019-01-29 18:38:45 +0800

    合同bug修复

--- crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/ContractServiceImpl.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/ContractServiceImpl.java
@@ -17,7 +17,6 @@ import com.jaagro.crm.biz.entity.Customer;
 import com.jaagro.crm.biz.entity.CustomerContract;
 import com.jaagro.crm.biz.entity.TruckType;
 import com.jaagro.crm.biz.mapper.*;
-import com.jaagro.crm.biz.service.MessageClientService;
 import com.jaagro.crm.biz.service.OssSignUrlClientService;
 import com.jaagro.utils.ResponseStatusCode;
 import com.jaagro.utils.ServiceResult;
@@ -33,6 +32,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -75,6 +75,7 @@ public class ContractServiceImpl implements ContractService {
     private CustomerSiteMapperExt siteMapperExt;
     @Autowired
     private TruckTypeMapperExt truckTypeMapperExt;
+
     /**
      * 创建合同
      *
@@ -130,33 +131,13 @@ public class ContractServiceImpl implements ContractService {
      * @param date2
      * @return
      */
-    public static int differentDays(Date date1, Date date2) {
-        Calendar cal1 = Calendar.getInstance();
-        cal1.setTime(date1);
-
-        Calendar cal2 = Calendar.getInstance();
-        cal2.setTime(date2);
-        int day1 = cal1.get(Calendar.DAY_OF_YEAR);
-        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
-
-        int year1 = cal1.get(Calendar.YEAR);
-        int year2 = cal2.get(Calendar.YEAR);
-        //同一年
-        if (year1 != year2) {
-            int timeDistance = 0;
-            for (int i = year1; i < year2; i++) {
-                //闰年
-                if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
-                    timeDistance += 366;
-                    //不是闰年
-                } else {
-                    timeDistance += 365;
-                }
-            }
-            return timeDistance + (day2 - day1);
-            //不同年
-        } else {
-            return day2 - day1;
+    public static int differentDays(Date date2, Date date1) {
+        try {
+            int a = (int) ((date1.getTime() - date2.getTime()) / (1000 * 3600 * 24));
+            return a;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return 1;
         }
     }
 
