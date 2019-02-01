b0f329176a5ab125523dbc84de3409c856cb12b8
yogasan <yoga330356233@126.com>
2019-02-01 10:25:54 +0800

    redis事务

---
+++ crm-biz/src/main/java/com/jaagro/crm/biz/service/impl/TestServiceImpl.java
@@ -0,0 +1,66 @@
+package com.jaagro.crm.biz.service.impl;
+
+import com.jaagro.crm.api.service.TestService;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+/**
+ * @author yj
+ * @date 2019/1/31 15:58
+ */
+@Service
+@Slf4j
+public class TestServiceImpl implements TestService {
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+    @Override
+    @Transactional
+    public void testDelTransactional(String token, String wxId, String userId) {
+        //redisTemplate.multi();
+        redisTemplate.delete(token);
+        redisTemplate.delete(wxId);
+        if (userId.equals("ee")){
+            throw new RuntimeException("抛出异常");
+        }
+        redisTemplate.delete(userId);
+        //redisTemplate.exec();
+    }
+
+    @Override
+    public void testDelTransactionUseOneConnection(String token, String wxId, String userId) {
+        final String tokenF = token;
+        final String wxIdF = wxId;
+        final String userIdF = userId;
+        Long delFromRedis = 3L;
+        Boolean execute = redisTemplate.execute((RedisCallback<Boolean>) connection -> {
+            byte[] bytesToken = new byte[1];
+            if (StringUtils.hasText(tokenF)){
+                bytesToken = tokenF.getBytes();
+            }
+            byte[] bytesWxId = new byte[1];
+            if (StringUtils.hasText(wxIdF)){
+                bytesWxId = wxIdF.getBytes();
+            }
+            byte[] bytesUserId = new byte[1];
+            if (StringUtils.hasText(userIdF)){
+                bytesUserId = userIdF.getBytes();
+            }
+            Long del = connection.del(bytesToken, bytesWxId, bytesUserId);
+            if (del != null && del.equals(delFromRedis)) {
+                return true;
+            }
+            return false;
+        });
+        if (execute == null || !execute){
+            log.warn("invalidateToken fail token={},userId={},wxId={}",token,userId,wxId);
+        }
+    }
+
+
+}
