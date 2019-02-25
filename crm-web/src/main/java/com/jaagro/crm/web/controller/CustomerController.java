41f2067c660507856951bf914700a94f016248ce
gaoxin <1506645910@qq.com>
2019-02-25 15:30:45 +0800

    merging

--- crm-web/src/main/java/com/jaagro/crm/web/controller/CustomerController.java
+++ crm-web/src/main/java/com/jaagro/crm/web/controller/CustomerController.java
@@ -303,7 +303,7 @@ public class CustomerController {
 
     @ApiOperation("根据关键字查询客户id集合")
     @GetMapping("/listCustomerIdByKeyWord/{keyword}")
-    public BaseResponse<List<Integer>> getContactsById(@PathVariable String keyword) {
+    public BaseResponse<List<Integer>> listCustomerIdByKeyWord(@PathVariable String keyword) {
         return BaseResponse.successInstance(customerContactsService.listCustomerIdByKeyWord(keyword));
     }
 }
