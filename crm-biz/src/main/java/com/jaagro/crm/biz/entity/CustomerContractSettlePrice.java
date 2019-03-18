74bf03c03cc4fcfd8d86823bf9ddef8380a5aab7
baiyiran <1169172027@qq.com>
2019-03-18 11:21:37 +0800

    合同报价添加饲料包装类型

--- crm-biz/src/main/java/com/jaagro/crm/biz/entity/CustomerContractSettlePrice.java
+++ crm-biz/src/main/java/com/jaagro/crm/biz/entity/CustomerContractSettlePrice.java
@@ -60,6 +60,11 @@ public class CustomerContractSettlePrice implements Serializable{
     private Integer goodsType;
 
     /**
+     * 饲料类型：1-散装 2-袋装 (仅饲料情况下)
+     */
+    private Integer feedType;
+
+    /**
      * 里程数
      */
     private BigDecimal mileage;
